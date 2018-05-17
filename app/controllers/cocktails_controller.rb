493f739619fccba2d4cdb30c506d500a4bef4641
Dania Maciel <daniaemily@gmail.com>
2018-05-17 16:12:32 +0200

    mister cocktail app

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,36 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    # initialize a new instance of Cocktail with the params from my form
+    @cocktail = Cocktail.new(cocktail_params)
+    # save the new cocktail to the DB
+      if @cocktail.save
+      # redirect to the show view of the newly created cocktail
+        redirect_to cocktail_path(@cocktail)
+      else
+        render :new
+      end
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktail_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
